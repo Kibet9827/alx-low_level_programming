b34d8be47f7bfc4e8d5357443e368a58913cc50f
root <kibet9827>
2023-06-17 02:35:45 -0700

    printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Description : a program that prints using function printf()
+ *
+ * Return: Always 09sucess)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
