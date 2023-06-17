fab467e670b532d73ab0cb6169f653e37a6ee24e
root <kibet9827>
2023-06-16 23:10:39 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,8 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle);
+#include <stdio.h>
+/*writing c program tha prints a line using puts*/
+int main(){
+	char string[] = "puts() function in c";
+	
+	puts("Programming is like building a multilingual puzzle);
 return(0);
+}
