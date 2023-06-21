a55b8c0193e80ae0adb21b7285d669fb3f3b1edd
root <kibet9827>
2023-06-21 00:44:07 -0700

    _isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/*
+ * _isalpha: checks for alphabet character
+ * @c: character to be checked
+ * Return: 1 if character is uppercase or 0 for other case
+ */
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 96) || (c <= 97 && c <= 122))
+	{
+		return (1);
+	}
+		return (0);
+}
