effe50d1644b69dbf8aa36b2f05206ce0a6ce858
root <kibet9827>
2023-07-01 16:26:37 -0700

    5-printf.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/*
+ * Main - Entry point
+ *
+ * Description: c program that prints various types
+ * on the computer that is compiled and run on
+ *
+ * Return: Always 0(success)
+ */
+
+int main(void)
+{
+	printf("size of a char : %d bytes(s)\n");
+	printf("size of an int : %d bytes(s)\n");
+	printf("size of a long int : %d bytes(s)\n");
+	printf("size of a long long int : %d bytes(s)\n");
+	printf("size of a float : %d bytes(s)\n");
+
+	return (0);
+}
