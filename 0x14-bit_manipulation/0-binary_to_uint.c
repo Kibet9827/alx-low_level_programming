d221a746e1f22303edbc600f4be2241430ad434b
root <kibet9827>
2023-08-04 02:06:43 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 		}
 		for (i = 0; b[i]; i++)
 		{
-			result = 2* result + (b[i] - '0');
+			result = 2 * result + (b[i] - '0');
 		}
 
 		return (result);
