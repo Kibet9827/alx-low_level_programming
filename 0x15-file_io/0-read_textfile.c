69b52ecd158e75e656758b87f4600229e2af1ae7
root <kibet9827>
2023-08-07 23:46:08 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,8 +4,8 @@
 /**
  *read_textfile - function that reads text file
  *
- *@Requiescat: the text file
- *@letters: number of letter to be read 
+ *@filename: the text file
+ *@letters: number of letter to be read
  *Return: res - number of bytes read and printed
  *        0 when function fails or filename is null
  *
@@ -19,18 +19,16 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
-	
+
 	if (fd == -1)
 		return (0);
 
-	buf = malloc(sizeof(char) *letters);
+	buf = malloc(sizeof(char) * letters);
 	t = read(fd, buf, letters);
 	res = write(STDOUT_FILENO, buf, t);
 
-	free (buf);
-	close (fd);
+	free(buf);
+	close(fd);
 
-
-		return (res);
+	return (res);
 }
-	
