ccbf2bce935de79470e175b1c03297eb74883b46
root <kibet9827>
2023-08-08 12:04:44 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,9 +19,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content == NULL)
+	if (text_content != NULL)
+	{
 		for (len = 0; text_content[len];)
 			len++;
+	}
 	fd = open(filename, O_RDONLY | O_CREAT | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
