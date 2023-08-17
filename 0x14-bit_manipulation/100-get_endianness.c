304c16cd16d040016e2dffb7470d2eb90f07a112
root <kibet9827>
2023-08-17 12:29:57 -0700

    check if its big or little endian

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * get_endianess - checks if the byte is little-endian or big-endian
+ * get_endianness - checks if the byte is little-endian or big-endian
  *
- * Rturn: 0 for big 1 for little
+ * Return: 0 for big 1 for little
  */
 
 int get_endianness(void)
