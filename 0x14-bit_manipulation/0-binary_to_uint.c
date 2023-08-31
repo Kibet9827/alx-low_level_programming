528ca29ae5194569c207fa122a15e66df62772fe
kibet9827 <kibetkeiyo@gmail.com>
2023-08-31 03:30:35 -0700

    0-binary_to_uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,12 +12,14 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int int_val = 0;
 	int i;
 
-		if (!b)
+		if  (!b)
 		{
 			return (0);
 		}
 		for (i = 0; b[i]; i++)
 		{
+			if (b[i] < '0' || b[i] > '1')
+				return (0);
 			int_val = 2 * int_val + (b[i] - '0');
 		}
 
